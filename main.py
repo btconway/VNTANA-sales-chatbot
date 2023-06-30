154d648d027944a66c1c129698b6e34c47c23a90
btconway <ben@vntana.com>
2023-06-30 10:06:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ from langchain.prompts.chat import (
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.embeddings import OpenAIEmbeddings
+import logging
+import langchain
+import os
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.cache import SQLiteCache
+
+logging.basicConfig(level=logging.INFO)
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+# Replace RedisSemanticCache with SQLiteCache
+langchain.llm_cache = SQLiteCache(database_path=".langchain.db")
 
 # Read the prompt template from a .txt file
 with open('/Users/benconway/Documents/GitHub/VNTANA-sales-chatbot/Anthropic_Prompt.txt', 'r') as file:
@@ -25,13 +38,13 @@ memory = ConversationBufferMemory(memory_key="chat_history")
 # Initialize the LLMChain with the ChatAnthropic model
 llm_chain = LLMChain(
     llm=ChatAnthropic(
-    model="claude-v1.3-100k",
-    temperature=0.4,
-    max_tokens_to_sample=75000,
-    streaming=True,
-    verbose=False,
-    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-),
+        model="claude-v1.3-100k",
+        temperature=0.4,
+        max_tokens_to_sample=75000,
+        streaming=True,
+        verbose=False,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    ),
     prompt=prompt,
     verbose=False,
     memory=memory,
@@ -43,4 +56,5 @@ while True:
     if human_input.lower() == "quit":
         break
     response = llm_chain.predict(human_input=human_input)
-    print("AI: " + response)
+    print(response)
+    # Don't print here as it's also printed in the callback
